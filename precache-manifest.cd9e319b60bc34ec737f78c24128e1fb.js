692963fa3692ce5fd10c580d8b16c768f5beb4b5
kyrillosezzat <kyrillosezzat300@gmail.com>
2019-08-27 02:58:18 -0700

    Updates

---
+++ precache-manifest.cd9e319b60bc34ec737f78c24128e1fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "0a6d64b8d68e734ed541",
+    "url": "/TodoList/static/js/main.8dc8998a.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/TodoList/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "0a6d64b8d68e734ed541",
+    "url": "/TodoList/static/css/main.c75ed5af.chunk.css"
+  },
+  {
+    "revision": "3e873b19786e01cd4550741631dab1b4",
+    "url": "/TodoList/index.html"
+  }
+];
